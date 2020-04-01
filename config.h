22ec02fb2c49356c3e7a9e730de5fcc1b87d7f34
Lander Moerkerke <lander.moerkerke@telenet.be>
2020-04-01 23:53:53 +0200

    Switch to altkey for quit

--- config.h
+++ config.h
@@ -147,7 +147,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ ALTKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */

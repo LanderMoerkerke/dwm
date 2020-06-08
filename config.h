c84cf8467dad530d6759bbc67fd64315949e1b82
Lander Moerkerke <lander.moerkerke@telenet.be>
2020-06-08 20:46:47 +0200

    Add swallow patch

--- config.h
+++ config.h
@@ -13,6 +13,8 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+
 static const int showsystray        = 1;     /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
@@ -57,16 +59,19 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       2,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,       "Spotify",  NULL,       7,            1,           -1 },
-	{ "Gimp",	  NULL,		  NULL,	      0,			1,			 -1 },
-	{ "Firefox",  NULL,		  NULL,	      1 << 8,		0,			 -1 },
-	{ NULL,		  "sp-tmux",  NULL,	      SPTAG(0),		1,			 -1 },
-	{ NULL,		  "sp-lf",	  NULL,	      SPTAG(1),		1,			 -1 },
-	{ NULL,		  "sp-math",  NULL,	      SPTAG(2),		1,			 -1 },
-	{ NULL,		  "caprine",  NULL,	      SPTAG(3),		1,			 -1 },
+	/* class      instance    title             tags mask     isfloating  isterminal   noswallow  monitor  */
+	{ "Gimp",     NULL,       NULL,             2,            0,          0,           0,         -1 },
+	{ "Firefox",  NULL,       NULL,             1 << 8,       0,          0,           0,         -1 },
+	{ NULL,       "Spotify",  NULL,             7,            1,          0,           0,         -1 },
+	{ "Gimp",	  NULL,		  NULL,	            0,			  1,		  0,           0,         -1 },
+	{ "Firefox",  NULL,		  NULL,	            1 << 8,		  0,		  0,           0,         -1 },
+    { "St",       NULL,       NULL,       	    0,            0,          1,           0,         -1 },
+    { "URxvt",    NULL,       NULL,       	    0,            0,          1,           0,         -1 },
+	{ NULL,       NULL,       "Event Tester",   0,            0,          0,           1,         -1 },
+	{ NULL,		  "sp-tmux",  NULL,	            SPTAG(0),	  1,		  0,           0,         -1 },
+	{ NULL,		  "sp-lf",	  NULL,	            SPTAG(1),	  1,		  0,           0,         -1 },
+	{ NULL,		  "sp-math",  NULL,	            SPTAG(2),	  1,		  0,           0,         -1 },
+	{ NULL,		  "caprine",  NULL,	            SPTAG(3),	  1,		  0,           0,         -1 },
 };
 
 /* layout(s) */

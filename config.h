6b675f5b2f4d7a2bdb84de7bc9dfa2d1022ac561
Lander Moerkerke <lander.moerkerke@telenet.be>
2023-09-16 17:50:07 +0200

    Extend scratchpad

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"urxvtc", "-name", "sp-tmux", "-geometry", "120x50", "-e", "scratch-tmux", NULL };
+const char *spcmd1[] = {"urxvtc", "-name", "sp-tmux", "-geometry", "230x60", "-e", "scratch-tmux", NULL };
 const char *spcmd2[] = {"urxvtc", "-name", "sp-lf",   "-geometry", "144x41", "-e", "lf", NULL };
 const char *spcmd3[] = {"urxvtc", "-name", "sp-math", "-geometry", "100x40", "-e", "scratch-math", NULL };
 const char *spcmd4[] = {"caprine", NULL };
